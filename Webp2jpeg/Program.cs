a8678c6bb58837a752e592f4040692efbaf45f24
Alexey Mironov <amironov73@gmail.com>
2021-10-28 07:47:01 +0800

    Add Webp2jpeg utility

---
+++ Webp2jpeg/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+using WebPWrapper;
+using WebPWrapper.Decoder;
+
+class Program
+{
+    static int Main (string[] args)
+    {
+        if (args.Length is < 1 or > 2)
+        {
+            Console.Error.WriteLine ("Usage: webp-to-jpeg <input> [output]");
+            return 1;
+        }
+
+        try
+        {
+
+            var inputFileName = args[0];
+            var outputFileName = args.Length > 1
+                ? args[1]
+                : Path.ChangeExtension (inputFileName, ".jpg");
+
+            WebPExecuteDownloader.Download();
+
+            var builder = new WebPDecoderBuilder();
+            var decoder = builder.Build();
+
+            using var output = File.Create (outputFileName);
+            using var input = File.OpenRead (inputFileName);
+            decoder.Decode (input, output);
+
+        }
+        catch (Exception exception)
+        {
+            Console.Error.WriteLine ($"Exception: {exception.Message}");
+            return 1;
+        }
+
+        return 0;
+    }
+}
