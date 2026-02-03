9a9ce65dbe1694982f92bba7204950987790679e
Alexey Mironov <amironov73@gmail.com>
2026-02-03 11:40:36 +0800

    Improve ltx2_preconfig

--- LTX2_Helper/ltx2_preconfig.py
+++ LTX2_Helper/ltx2_preconfig.py
@@ -10,27 +10,24 @@ class LTX2_PreConfig:
                                    "544 x 960 (Vertical)",
                                    "1024 x 576 (HD-ish)"
                                ],),
-                "target_frames": ("INT", {"default": 65, "min": 1, "max": 257, "step": 1}),
+                "fps": ("INT", {"default": 24, "min": 1, "max": 60, "step": 1}),
+                "seconds": ("INT", {"default": 3, "min": 1, "max": 20, "step": 1}),
             }
         }
 
-    RETURN_TYPES = ("INT", "INT", "INT")
-    RETURN_NAMES = ("width", "height", "frames")
+    RETURN_TYPES = ("INT", "INT", "INT", "INT", "INT")
+    RETURN_NAMES = ("width", "height", "FPS", "seconds", "frames")
     FUNCTION = "calculate"
     CATEGORY = "LTX2/Config"
 
-    def calculate(self, resolution, target_frames):
+    def calculate(self, resolution, fps, seconds):
         # Парсим строку разрешения
         res_part = resolution.split(" (")[0] # Убираем текст в скобках
         width, height = map(int, res_part.split(" x "))
-
-        # Формула для кадров: кратность 8 + 1
-        # (x // 8) * 8 + 1 гарантирует, что 64 станет 65, а 60 станет 57 (или 65)
-        # Для предсказуемости используем round:
-        frames = ((target_frames - 1) // 8) * 8 + 1
+        frames = (seconds * fps // 8) * 8 + 1
         if frames < 1: frames = 1
 
-        return (width, height, frames)
+        return (width, height, fps, frames)
 
 NODE_CLASS_MAPPINGS = {"LTX2_PreConfig": LTX2_PreConfig}
 NODE_DISPLAY_NAME_MAPPINGS = {"LTX2_PreConfig": "🚀 LTX2 PreConfig"}
