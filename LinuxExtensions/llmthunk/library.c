8d7e903d391e24a152d938fbbf09fc658b248532
Alexey Mironov <amironov73@gmail.com>
2026-04-21 13:00:35 +0800

    Working on llmthunk: Improve Run function

--- LinuxExtensions/llmthunk/library.c
+++ LinuxExtensions/llmthunk/library.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -40,6 +41,28 @@ EXPORT int DLL_CALL Slashes
     return 0;
 }
 
+NO_EXPORT int can_execute
+    (
+        struct stat *st
+    )
+{
+    uid_t uid = geteuid(); // Получаем ID текущего пользователя
+
+    // 1. Если мы — владелец файла
+    if (uid == st->st_uid) {
+        return st->st_mode & S_IXUSR;
+    }
+
+    // 2. Если мы входим в группу владельцев файла
+    gid_t gid = getegid();
+    if (gid == st->st_gid) {
+        return st->st_mode & S_IXGRP;
+    }
+
+    // 3. Для всех остальных
+    return st->st_mode & S_IXOTH;
+}
+
 // Экспортированная функция
 EXPORT int DLL_CALL Run
     (
@@ -48,15 +71,25 @@ EXPORT int DLL_CALL Run
         int size
     )
 {
-    NOT_USED (input);
-
     const char *prefix_dir = "workdir/iogunb";
+    const char *llmcall = "./llmcall";
     uuid_t binuuid;
     char uuid_str[37];
     char input_filename[64];
     char output_filename[64];
     char command_line[256];
+    struct stat stat_buffer;
     int rc;
+
+    if (stat (llmcall, &stat_buffer)) {
+        snprintf (command_line, sizeof (command_line), "Can't stat file %s", llmcall);
+        return SayError (output, size, command_line);
+    }
+
+    if (!can_execute (&stat_buffer)) {
+        snprintf (command_line, sizeof (command_line), "Not executable: %s", llmcall);
+        return SayError (output, size, command_line);
+    }
 
     mkdir (prefix_dir, 0755);
 
@@ -68,37 +101,52 @@ EXPORT int DLL_CALL Run
     snprintf (input_filename, sizeof (input_filename), "%s/input_%s.txt", prefix_dir, uuid_str);
     snprintf (output_filename, sizeof (output_filename), "%s/output_%s.txt", prefix_dir, uuid_str);
 
-    FILE *inputFile = fopen (input_filename, "wt");
+    FILE *inputFile = fopen (input_filename, "w");
     if (!inputFile) {
-        return SayError (output, size, "Can't open input file");
+        snprintf (command_line, sizeof (command_line), "Can't create input file %s", input_filename);
+        return SayError (output, size, command_line);
     }
 
     int length = strlen (input);
-    fwrite (input, 1, length, inputFile);
+    rc = fwrite (input, 1, length, inputFile);
     fclose (inputFile);
+    if (rc != length) {
+        snprintf (command_line, sizeof (command_line), "Can't write input file %s", input_filename);
+        return SayError (output, size, command_line);
+    }
 
     memset (command_line, 0, sizeof (command_line));
-    snprintf (command_line, sizeof (command_line), "%s -i %s -o %s",
-        "./llmcall", input_filename, output_filename);
+    snprintf
+        (
+            command_line,
+            sizeof (command_line),
+            "%s -i %s -o %s",
+            llmcall,
+            input_filename,
+            output_filename
+        );
 
     rc = system (command_line);
     if (rc != 0) {
-        return SayError (output, size, "Error executing command");
+        snprintf (command_line, sizeof (command_line), "Can't execute %s", llmcall);
+        return SayError (output, size, command_line);
     }
 
     memset (output, 0, size);
-    FILE *outputFile = fopen (output_filename, "rt");
+    FILE *outputFile = fopen (output_filename, "r");
     if (!outputFile) {
-        return SayError (output, size, "Can't open output file");
+        snprintf (command_line, sizeof (command_line), "Can't open output file %s", output_filename);
+        return SayError (output, size, command_line);
     }
 
     if (outputFile != NULL) {
         rc = fread (output, 1, size - 1, outputFile);
-        if (rc <= 0) {
-            return SayError (output, size, "Error reading output file");
-        }
-
         fclose (outputFile);
+
+        if (rc <= 0) {
+            snprintf (command_line, sizeof (command_line), "Error reading output file %s", output_filename);
+            return SayError (output, size, command_line);
+        }
     }
 
     return 0;
